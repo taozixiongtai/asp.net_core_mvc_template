fe30ef12db4610c14fe0d21a3e3623b3a9ab08a1
taozixiongtai <34221849+taozixiongtai@users.noreply.github.com>
2019-04-06 18:29:54 +0800

    添加项目文件。

---
+++ mymvc_core/Data/Migrations/20190403072823_InitialCreate1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mymvc_core.Data.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ mymvc_core/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mymvc_core.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace mymvc_core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)        //这就是依赖注入，我来说明一下，这个类他需要一个配置（configuration），但是他不自己去创建，而是使用构造函数去让别人给他创建，又因为设计模式的问题，他定义了接口方便别人去调用，这就是依赖注入
+        {
+            Configuration = configuration;      //这个是去配置文件里面读取配置。
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // 使用这个方法去添加服务到容器里面,所以你可以在这里面看到很多添加方法，然后可以在Configure方法里面去调用
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            var connection = Configuration.GetConnectionString("MysqlConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connection));
+           
+            
+    
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // 使用这方法去配置HTTP请求管道，这里全是中间件
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
